5ee04a5ccf96b3a79bf76b67634f9cf451e31bf8
HtetThuYaAungg <htetthuya.htya@gmail.com>
2024-09-05 12:03:21 +0630

    [ENHANCEMENT] change production url

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.enableCors({
     origin:
       process.env.NODE_ENV === 'production'
-        ? process.env.PRODUCTION_UTL
+        ? process.env.PRODUCTION_URL
         : ['http://localhost:3000', 'http://localhost:3001'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allow cookies to be sent
